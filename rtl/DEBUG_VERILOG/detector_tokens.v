b5f18a50135815242f0d753b1269bf4e9679ba9f
Felipe Fernandes <felipe.fernandes.costa99@gmail.com>
2017-05-22 01:37:23 -0300

    Trying corrections to detection right token.

--- rtl/DEBUG_VERILOG/detector_tokens.v
+++ rtl/DEBUG_VERILOG/detector_tokens.v
@@ -34,7 +34,8 @@ module detector_tokens(
 								input  rx_din,
 								input  rx_sin,
 								input  rx_resetn,
-								output [13:0] info
+								output reg rx_buffer_write,
+								output reg [13:0] info
 							 );
 			
 	reg rx_error;
@@ -90,28 +91,29 @@ module detector_tokens(
 	//wire check_c_d;
 
 	reg rx_data_take;
+	reg rx_data_take_0;
 
+	//wire [13:0] info_w;
 	reg first_time;
 
-	reg ready_control;
-	reg ready_data;
-
+	wire ready_control;
+	wire ready_data;
+	
+	
 	//CLOCK RECOVERY
-	assign posedge_clk 	= (rx_din ^ rx_sin)?1'b1:1'b0;
-	assign negedge_clk 	= (!first_time)?1'b0:(!(rx_din ^ rx_sin))?1'b1:1'b0;
+	assign posedge_clk 	= (!rx_resetn)?1'b0:(rx_din ^ rx_sin)?1'b1:1'b0;
+	assign negedge_clk 	= (!rx_resetn)?1'b0:(!first_time)?1'b0:(!(rx_din ^ rx_sin))?1'b1:1'b0;
 
 	assign rx_got_bit       = (posedge_clk)?1'b1:1'b0;
-	
-	assign info = {control_l_r,control,rx_error,rx_got_bit,rx_got_null,rx_got_nchar,rx_got_time_code,rx_got_fct}; 
+		
+	assign ready_control    = (negedge_clk & counter_neg == 5'd2 && bit_c_2)?1'b1:1'b0;
+	assign ready_data       = (negedge_clk & counter_neg == 5'd5)?1'b1:1'b0;
 
 always@(posedge posedge_clk or negedge rx_resetn)
 begin
 
 	if(!rx_resetn)
 	begin
-		bit_c_1 <= 1'b0;
-		bit_c_3 <= 1'b0;
-
 		bit_d_1 <= 1'b0;
 		bit_d_3 <= 1'b0;
 		bit_d_5 <= 1'b0;
@@ -121,9 +123,6 @@ begin
 	end
 	else
 	begin
-		bit_c_1 <= rx_din;
-		bit_c_3 <= bit_c_1;
-
 		bit_d_1 <= rx_din;
 		bit_d_3 <= bit_d_1;
 		bit_d_5 <= bit_d_3;
@@ -135,14 +134,28 @@ begin
 
 end
 
+always@(posedge posedge_clk or negedge rx_resetn)
+begin
+
+	if(!rx_resetn)
+	begin
+		bit_c_1 <= 1'b0;
+		bit_c_3 <= 1'b0;
+	end
+	else
+	begin
+		bit_c_1 <= rx_din;
+		bit_c_3 <= bit_c_1;
+	end
+
+end
+
+
 always@(posedge negedge_clk or negedge rx_resetn)
 begin
 
 	if(!rx_resetn)
 	begin
-		bit_c_0 <= 1'b0;
-		bit_c_2 <= 1'b0;
-
 		bit_d_0 <= 1'b0;
 		bit_d_2 <= 1'b0;
 		bit_d_4 <= 1'b0;
@@ -150,23 +163,59 @@ begin
 		bit_d_8 <= 1'b0;
 	end
 	else
+	begin	
+		bit_d_0 <= rx_din;
+		bit_d_2 <= bit_d_0;
+		bit_d_4 <= bit_d_2;
+		bit_d_6 <= bit_d_4;
+		bit_d_8 <= bit_d_6;
+	end
+end
+
+
+always@(posedge negedge_clk or negedge rx_resetn)
+begin
+
+	if(!rx_resetn)
+	begin
+		bit_c_0 <= 1'b0;
+		bit_c_2 <= 1'b0;
+	end
+	else
 	begin
 	
-		if(!first_time)
-		begin
-		end
-		else
-		begin
-			bit_c_0 <= rx_din;
-			bit_c_2 <= bit_c_0;
-
-			bit_d_0 <= rx_din;
-			bit_d_2 <= bit_d_0;
-			bit_d_4 <= bit_d_2;
-			bit_d_6 <= bit_d_4;
-			bit_d_8 <= bit_d_6;
-		end
+		bit_c_0 <= rx_din;
+		bit_c_2 <= bit_c_0;
 	end
+end
+
+
+always@(posedge ready_control or negedge rx_resetn)
+begin
+
+	if(!rx_resetn)
+	begin
+		control_r <= 4'd0;
+	end
+	else
+	begin
+		control_r <= {bit_c_3,bit_c_2,bit_c_1,bit_c_0};
+	end		
+end
+
+
+always@(posedge ready_data or negedge rx_resetn)
+begin
+
+	if(!rx_resetn)
+	begin
+		dta_timec <= 10'd0;
+	end
+	else
+	begin
+		dta_timec <= {bit_d_9,bit_d_8,bit_d_0,bit_d_1,bit_d_2,bit_d_3,bit_d_4,bit_d_5,bit_d_6,bit_d_7};
+	end
+			
 end
 
 
@@ -182,11 +231,11 @@ begin
 	end
 	else
 	begin
-		if(!first_time)
-		begin
-		end
-		else
-		begin
+		//if(!first_time)
+		//begin
+		//end
+		//else
+		//begin
 			if(counter_neg == 5'd1)
 			begin
 				if(bit_c_0)
@@ -230,80 +279,134 @@ begin
 					counter_neg <= counter_neg + 5'd1;
 				end
 			end	
-		end
+		//end
 	end
 end
 
-
-always@(*)
+always@(posedge negedge_clk or negedge rx_resetn)
 begin
 
-	rx_got_fct = 1'b0;
-
-	if(control_l_r[2:0] != 3'd7 && control[2:0] == 3'd4 && ready_control)
+	if(!rx_resetn)
 	begin
-		rx_got_fct = 1'b1;
+		rx_error <= 1'b0;
+	end	
+	else
+	begin
+		if(last_is_control)
+		begin
+			if(last_was_control)
+			begin
+				if(!(control[2]^control_l_r[0]^control_l_r[1]) != control[3])
+				begin
+					rx_error <= 1'b1;
+				end
+			end
+			else if(last_was_timec)
+			begin
+				if(!(control[2]^timecode[0]^timecode[1]^timecode[2]^timecode[3]^timecode[4]^timecode[5]^timecode[6]^timecode[7])  != control[3])
+				begin
+					rx_error <= 1'b1;
+				end
+			end
+			else if(last_was_data)
+			begin
+				if(!(control[2]^data[0]^data[1]^data[2]^data[3]^data[4]^data[5]^data[6]^data[7]) != control[3])
+				begin
+						rx_error <= 1'b1;
+				end
+			end
+
+			end
+			else if(last_is_data)
+			begin
+				if(last_was_control)
+				begin
+					if(!(data[8]^control[1]^control[0]) != data[9])
+					begin
+						rx_error <= 1'b1;
+					end
+				end
+				else if(last_was_timec)
+				begin
+					if(!(data[8]^timecode[0]^timecode[1]^timecode[2]^timecode[3]^timecode[4]^timecode[5]^timecode[6]^timecode[7])  != data[9])
+					begin
+						rx_error <= 1'b1;
+					end
+				end
+				else if(last_was_data)
+				begin
+					if(!(data[8]^data[0]^data_l_r[1]^data_l_r[2]^data_l_r[3]^data_l_r[4]^data_l_r[5]^data_l_r[6]^data_l_r[7]) != data[9])
+					begin
+						rx_error <= 1'b1;
+					end
+				end
+			end
+		end
+
+end
+
+always@(posedge negedge_clk or negedge rx_resetn)
+begin
+	if(!rx_resetn)
+	begin
+		rx_got_null 	  <= 1'b0;
+		rx_got_nchar 	  <= 1'b0;
+		rx_got_time_code  <= 1'b0;			
+		rx_got_fct 	  <= 1'b0;
 	end
 	else
 	begin
-		rx_got_fct = 1'b0;
-	end
-
-end
-
-always@(*)
-begin
-
-	rx_got_null = 1'b0;
-
-	if(control_l_r[2:0] == 3'd7 && control[2:0] == 3'd4 && ready_control)
-	begin
-		rx_got_null = 1'b1;
-	end
-end
-
-always@(*)
-begin
-
-	rx_got_time_code = 1'b0;
-	rx_got_nchar = 1'b0;
-
- 	if(control[2:0] != 3'd7 && is_data )
-	begin
-		rx_got_nchar = 1'b1;
-	end
-	
-	if(control[2:0] == 3'd7 && is_data)
-	begin
-		rx_got_time_code = 1'b1;
-	end
-	
-end
-
-always@(counter_neg)
-begin
-
-	dta_timec     = 10'd0;
-	control_r     = 4'd0;
-	ready_control = 1'b0;
-	ready_data    = 1'b0;
-
-	if(negedge_clk)
+		if(control_l_r[2:0] != 3'd7 && control[2:0] == 3'd4 && last_is_control)
 		begin
-		if(counter_neg == 5'd2 && bit_c_2)
-		begin
-			control_r     = {bit_c_3,bit_c_2,bit_c_1,bit_c_0};
-			ready_control = 1'b1;
-			ready_data    = 1'b0;
+			rx_got_fct 	  <= 1'b1;
+			rx_got_null 	  <= 1'b0;
+			rx_got_nchar 	  <= 1'b0;
+			rx_got_time_code  <= 1'b0;			
 		end
-		else if(counter_neg == 5'd5)
+		else if(control[2:0] != 3'd7 && last_is_data)
 		begin
-			dta_timec     = {bit_d_9,bit_d_8,bit_d_0,bit_d_1,bit_d_2,bit_d_3,bit_d_4,bit_d_5,bit_d_6,bit_d_7};
-			ready_control = 1'b0;
-			ready_data    = 1'b1;
+			rx_got_nchar 	  <= 1'b1;
+			rx_got_null 	  <= 1'b0;
+			rx_got_time_code  <= 1'b0;			
+			rx_got_fct 	  <= 1'b0;
+		end
+		else if(control[2:0] == 3'd7 && last_is_data)
+		begin
+			rx_got_time_code  <= 1'b1;
+			rx_got_null 	  <= 1'b0;
+			rx_got_nchar 	  <= 1'b0;			
+			rx_got_fct 	  <= 1'b0;
+		end
+		else if(control_l_r[2:0] == 3'd7 && control[2:0] == 3'd4 && last_is_control)
+		begin
+			rx_got_null 	  <= 1'b1;
+			rx_got_nchar 	  <= 1'b0;
+			rx_got_time_code  <= 1'b0;			
+			rx_got_fct 	  <= 1'b0;
+		end
+		else
+		begin
+			rx_got_null 	  <= rx_got_null;
+			rx_got_nchar 	  <= rx_got_nchar;
+			rx_got_time_code  <= rx_got_time_code;			
+			rx_got_fct 	  <= rx_got_fct;
 		end
 	end
+end
 
+
+always@(posedge negedge_clk or negedge rx_resetn)
+begin
+	if(!rx_resetn)
+	begin
+		rx_buffer_write <=  1'b0;
+		rx_data_take_0  <=  1'b0;
+	end
+	else
+	begin
+		rx_data_take_0 <= rx_data_take;
+		rx_buffer_write  <= rx_data_take_0;
+	end
 end
 
 always@(posedge posedge_clk or negedge rx_resetn )
@@ -332,118 +435,28 @@ begin
 		last_was_data    <=1'b0;
 		last_was_timec   <=1'b0;
 		
+		info 		 <= 14'd0;
 		//rx_error  	 <= 1'b0;
 		//rx_got_null 	 <= 1'b0;
 		//rx_got_nchar 	 <= 1'b0;
 		//rx_got_time_code <= 1'b0;
 		//rx_got_fct 	 <= 1'b0;
+				
+		//meta_hold_setup    <= 1'b0;
+		//meta_hold_setup_n  <= 1'b0;
+		//meta_hold_setup_n_n<= 1'b0;
 
 	end
 	else
 	begin
 
-		if(last_is_control)
+		//meta_hold_setup_n_n <= meta_hold_setup;
+		//meta_hold_setup_n   <= meta_hold_setup_n_n; 
+
+		if(ready_control)
 		begin
-			if(last_was_control)
-			begin
-				if(!(control[2]^control_l_r[0]^control_l_r[1]) != control[3])
-				begin
-					rx_error <= 1'b1;
-				end
-			end
-			else if(last_was_timec)
-			begin
-				if(!(control[2]^timecode[0]^timecode[1]^timecode[2]^timecode[3]^timecode[4]^timecode[5]^timecode[6]^timecode[7])  != control[3])
-				begin
-					rx_error <= 1'b1;
-				end
-			end
-			else if(last_was_data)
-			begin
-				if(!(control[2]^data[0]^data[1]^data[2]^data[3]^data[4]^data[5]^data[6]^data[7]) != control[3])
-				begin
-					rx_error <= 1'b1;
-				end
-			end
-
-		end
-		else if(last_is_data)
-		begin
-			if(last_was_control)
-			begin
-				if(!(data[8]^control[1]^control[0]) != data[9])
-				begin
-					rx_error <= 1'b1;
-				end
-			end
-			else if(last_was_timec)
-			begin
-				if(!(data[8]^timecode[0]^timecode[1]^timecode[2]^timecode[3]^timecode[4]^timecode[5]^timecode[6]^timecode[7])  != data[9])
-				begin
-					rx_error <= 1'b1;
-				end
-			end
-			else if(last_was_data)
-			begin
-				if(!(data[8]^data[0]^data_l_r[1]^data_l_r[2]^data_l_r[3]^data_l_r[4]^data_l_r[5]^data_l_r[6]^data_l_r[7]) != data[9])
-				begin
-					rx_error <= 1'b1;
-				end
-			end
-		end
-
-		if((control_r[2:0] != 3'd7 && is_data) == 1'b1)
-		begin
-
-			data        	 	<= dta_timec;
-			data_l_r    	 	<= data;
-			
-			//rx_data_take <= 1'b1;
-			//rx_tick_out  <= 1'b0;
-
-			last_is_control  	<=1'b0;
-			last_is_data     	<=1'b1;
-			last_is_timec    	<=1'b0;
-			last_was_control 	<= last_is_control;
-			last_was_data    	<= last_is_data ;
-			last_was_timec 		<= last_is_timec;
-		end
-		else if((control_r[2:0] == 3'd7 && is_data) == 1'b1)
-		begin
-
-			timecode    		 <= dta_timec;
-			//rx_tick_out  <= 1'b1;
-			//rx_data_take <= 1'b0;
-
-			last_is_control  	<= 1'b0;
-			last_is_data     	<= 1'b0;
-			last_is_timec    	<= 1'b1;
-			last_was_control 	<= last_is_control;
-			last_was_data    	<= last_is_data ;
-			last_was_timec   	<= last_is_timec;
-		end
-		else if((control_r == 4'd6 || control_r == 4'd13 || control_r == 4'd5 || control_r == 4'd15 || control_r == 4'd7 || control_r == 4'd4 || control_r == 4'd12) && ready_control)
-		begin
-
 			control     	 <= control_r;
-			control_l_r 	 <= control[3:0];
-
-			if((control[2:0] == 3'd6) == 1'b1 )
-			begin
-				data <= 10'b0100000001;
-				//rx_data_take <= 1'b1;
-			end
-			else if(  (control[2:0] == 3'd5 ) == 1'b1 )
-			begin
-				data <= 10'b0100000000;
-			//	rx_data_take <= 1'b1;
-			end
-			else
-			begin
-			//	rx_data_take 	<= 1'b0;
-			end
-
-			//rx_tick_out  <= 1'b0;
+			control_l_r 	 <= control;
 
 			last_is_control 	 <= 1'b1;
 			last_is_data    	 <= 1'b0;
@@ -451,7 +464,70 @@ begin
 			last_was_control	 <= last_is_control;
 			last_was_data    	 <= last_is_data ;
 			last_was_timec   	 <= last_is_timec;
+
+		end	
+		else if(ready_data)
+		begin
+			if(control[2:0] != 3'd7)
+			begin
+				data        	<= dta_timec;
+				last_is_control  	<=1'b0;
+				last_is_data     	<=1'b1;
+				last_is_timec    	<=1'b0;
+				last_was_control 	<= last_is_control;
+				last_was_data    	<= last_is_data ;
+				last_was_timec 		<= last_is_timec;
+			end
+			else if(control[2:0] == 3'd7)
+			begin
+				timecode    	<= dta_timec;
+				last_is_control  	<= 1'b0;
+				last_is_data     	<= 1'b0;
+				last_is_timec    	<= 1'b1;
+				last_was_control 	<= last_is_control;
+				last_was_data    	<= last_is_data ;
+				last_was_timec   	<= last_is_timec;
+			end
 		end
+		else if(last_is_data)
+		begin
+
+			data_l_r    	 	<= data;
+					
+			info <= {control_l_r,control,rx_error,rx_got_bit,rx_got_null,rx_got_nchar,rx_got_time_code,rx_got_fct};		
+
+			rx_data_take <= 1'b1;
+			//rx_tick_out  <= 1'b0;
+							
+			//meta_hold_setup  <= 1'b0;
+		end
+		else if(last_is_timec)
+		begin
+			//rx_tick_out  <= 1'b1;
+			rx_data_take <= 1'b1;
+
+			info <= {control_l_r,control,rx_error,rx_got_bit,rx_got_null,rx_got_nchar,rx_got_time_code,rx_got_fct};							
+			
+			//meta_hold_setup  <= 1'b0;
+		end
+		else if(last_is_control)
+		begin
+
+			info <= {control_l_r,control,rx_error,rx_got_bit,rx_got_null,rx_got_nchar,rx_got_time_code,rx_got_fct};
+			
+			rx_data_take 	<= 1'b0;
+
+			if((control[2:0] == 3'd6) == 1'b1 )
+			begin
+				data <= 10'b0100000001;
+			end
+			else if(  (control[2:0] == 3'd5 ) == 1'b1 )
+			begin
+				data <= 10'b0100000000;
+			end
+
+		end
+		
 	end
 end					 
 							 
