3f2266e4d5b60c102e36e313d213755c4c38d756
Felipe Fernandes <felipe.fernandes.costa99@gmail.com>
2017-12-25 22:06:57 -0200

    Updating headers.

--- rtl/RTL_VB/bitc_capture_control.v
+++ rtl/RTL_VB/bitc_capture_control.v
@@ -1,3 +1,35 @@
+//+FHDR------------------------------------------------------------------------
+//Copyright (c) 2013 Latin Group American Integhrated Circuit, Inc. All rights reserved
+//GLADIC Open Source RTL
+//-----------------------------------------------------------------------------
+//FILE NAME	 :
+//DEPARTMENT	 : IC Design / Verification
+//AUTHOR	 : Felipe Fernandes da Costa
+//AUTHOR’S EMAIL :
+//-----------------------------------------------------------------------------
+//RELEASE HISTORY
+//VERSION DATE AUTHOR DESCRIPTION
+//1.0 YYYY-MM-DD name
+//-----------------------------------------------------------------------------
+//KEYWORDS : General file searching keywords, leave blank if none.
+//-----------------------------------------------------------------------------
+//PURPOSE  : ECSS_E_ST_50_12C_31_july_2008
+//-----------------------------------------------------------------------------
+//PARAMETERS
+//PARAM NAME		RANGE	: DESCRIPTION : DEFAULT : UNITS
+//e.g.DATA_WIDTH	[32,16]	: width of the data : 32:
+//-----------------------------------------------------------------------------
+//REUSE ISSUES
+//Reset Strategy	:
+//Clock Domains		:
+//Critical Timing	:
+//Test Features		:
+//Asynchronous I/F	:
+//Scan Methodology	:
+//Instantiations	:
+//Synthesizable (y/n)	:
+//Other			:
+//-FHDR------------------------------------------------------------------------
 module bit_capture_control(
 			input negedge_clk,
 			input posedge_clk,
