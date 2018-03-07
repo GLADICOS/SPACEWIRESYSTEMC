75b6f8a007f3254aa5f91567d0420223cfdac40a
ti-rodrigues <tiagofee@gmail.com>
2018-03-07 17:02:44 -0300

    Variables changed from 50Mhz to 200Mhz setting
    Variables of defines changed from 50Mhz to 200Mhz clock input

--- rtl/RTL_VA/defines.sv
+++ rtl/RTL_VA/defines.sv
@@ -18,13 +18,14 @@
 
 
 //Define Parameters
-//parameter DIV_64  = 12'd1280;
-//parameter DIV_128 = 12'd2560;
+//Parameters for CLOCK = 200Mhz
+`define DIV_64  				12'd1280;
+`define DIV_128 				12'd2560;
 
 //Parameters for CLOCK = 50Mhz
-`define DIV_64  				12'd320
-`define DIV_128 				12'd640
+//`define DIV_64  				12'd320
+//`define DIV_128 				12'd640
 
-//`define COUNTER_LIMIT 189 // 950ns - 200Mhz (nominal 850ns, accept (727ns - 1000ns)
-`define COUNTER_LIMIT		47
+`define COUNTER_LIMIT 189 // 950ns - 200Mhz (nominal 850ns, accept (727ns - 1000ns)
+//`define COUNTER_LIMIT 47 // 950ns - 50Mhz (nominal 850ns, accept (727ns - 1000ns)
 `define COUNTER_WIDTH		8
